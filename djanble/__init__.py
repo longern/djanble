e2344b205e45e2e6d40dfb65ebb2d191c2aa8f8c
longern <i@longern.com>
2021-02-12 11:45:54 +0800

    Disable admin log

--- djanble/__init__.py
+++ djanble/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.1.0"
+
+from django.contrib.admin.models import LogEntry
+
+from .managers import NoLogEntryManager
+
+# Change the model manager to one that doesn't log
+LogEntry.objects = NoLogEntryManager(LogEntry)
