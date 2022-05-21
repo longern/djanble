81bbacab351a559da811c282c97981730a8230a5
longern <771815005@qq.com>
2022-05-21 23:27:46 +0800

    Match delete-in statement

--- djanble/queries/delete.py
+++ djanble/queries/delete.py
@@ -3,12 +3,14 @@ import re
 
 
 def execute(conn: tablestore.OTSClient, sql: str, params):
-    delete_match = re.match('DELETE FROM "([^ ]*)" WHERE ".*"\\."id" = %s$', sql)
+    delete_regexp = r'\s*DELETE\s+FROM\s+"(?P<table>\S+?)"\s+WHERE\s+".*?"\."id"\s+(?:=\s*%s|IN\s+\(%s(,\s*%s)*\))\s*$'
+    delete_match = re.match(delete_regexp, sql)
     if not delete_match:
         raise ValueError(sql)
 
-    table_name = delete_match.groups()[0]
-    primary_keys = [("_partition", 0), ("id", params[0])]
-    consumed, return_row = conn.delete_row(
-        table_name, tablestore.Row(primary_keys), tablestore.Condition("EXPECT_EXIST")
-    )
+    table_name = delete_match.groupdict()["table"]
+    for param in params:
+        primary_keys = [("_partition", 0), ("id", param)]
+        consumed, return_row = conn.delete_row(
+            table_name, tablestore.Row(primary_keys), tablestore.Condition("EXPECT_EXIST")
+        )
